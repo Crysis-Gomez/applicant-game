c67eed2fec2064cb09eeffe05da44de76bbaa376
Johan Bloemberg <johan.bloemberg@spilgames.com>
2013-07-15 10:52:47 +0200

    This should work

--- swag/application/urls.py
+++ swag/application/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 # from django.views.generic import DetailView, ListView
-from application import views
+from swag.application import views
 
 # from polls.models import Poll
 # from django.utils import timezone
@@ -10,7 +10,7 @@ upload_url = settings.DBS_OPTIONS['base_url'][1:]
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^start_game/(?P<slug>[a-zA-Z\-0-9]+)/$', 'application.views.start_game', name='game'),
+    url(r'^start_game/(?P<slug>[a-zA-Z\-0-9]+)/$', views.start_game, name='game'),
     url(r'^game/(?P<unique_id>\w+)/$', views.play, name='play'),
     url(r'^(?P<unique_id>\w+)/game.js', views.gamejs, name='init'),
     url(r'^(?P<unique_id>\w+)/playerdata.js', views.playerdatajs, name='playerdata'),
--- swag/settings.py
+++ swag/settings.py
@@ -126,7 +126,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'swag.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
@@ -152,7 +152,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'application',
+    'swag.application',
     'south',
 )
 
